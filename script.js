be38b034b884ffa9e2c661ca5e395721d253d8aa
salaiaahu <salaiaahu@gmail.com>
2025-06-02 16:51:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3971,7 +3971,7 @@ async function initializeApp() {
     }
     try {
         statusMessage.textContent = 'Initializing SQL.js...';
-        SQL = await window.initSqlJs({ locateFile: file => `/baibalca/sql-wasm.wasm` });
+        SQL = await window.initSqlJs({ locateFile: file => `/${file}` });
         console.log('DEBUG: SQL.js initialized.');
         statusMessage.textContent = 'SQL.js initialized.';
         
